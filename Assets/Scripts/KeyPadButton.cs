51cef8a628877e69ae259ec6c8327a552ba992c6
feede07 <149623153+feede07@users.noreply.github.com>
2024-11-02 18:03:23 +0100

    objeto Keypad + primer boton

---
+++ Assets/Scripts/KeyPadButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    private void OnMouseDown(){
+        Debug.Log("Clicked me!");
+    }
+}
