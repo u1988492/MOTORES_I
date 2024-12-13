f780573b39761ec75d6e20a79746de3e6ebace49
u1988467 <160512346+u1988467@users.noreply.github.com>
2024-12-13 12:46:32 +0100

    new numpad + display

--- Assets/Scripts/DoorPuzzles/Keypad.cs
+++ Assets/Scripts/DoorPuzzles/Keypad.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Keypad : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class Keypad : MonoBehaviour
     public GameObject door;
     private int userInput = 0;
     public Collider interactionZone;
+    public TMP_Text showercode;
 
     private int pos = 0;
 
@@ -16,20 +19,27 @@ public class Keypad : MonoBehaviour
     {
         userInput = 0;
         pos = password_length-1;
+        showercode.text = "";
     }
 
     public void ButtonClicked(int number)
     {
+        Debug.Log("Tecla pulsada: " + number);
         if (number < 0 && pos < password_length - 1)
         {
             int factor = (int)Mathf.Pow(10, pos + 1);
             userInput -= (userInput / factor % 10) * factor;  // Eliminar el último dígito
             pos++;            // Recuperar una posición
+            if (showercode.text.Length > 0)
+            {
+                showercode.text = showercode.text.Substring(0, showercode.text.Length - 1);
+            }
         }
         else if(pos >= 0)
         {
             userInput += number * (int)Mathf.Pow(10, pos);
             pos--;
+            showercode.text += "*";
         }
 
         Debug.Log("Current password: " + userInput);
@@ -49,6 +59,7 @@ public class Keypad : MonoBehaviour
                 userInput = 0;
                 pos = password_length - 1;
             }
+            showercode.text = "";
         }
     }
 
